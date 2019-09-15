5da0df9f08459c28219a933448d10381e41d7ef9
cristian ermes morales <christianjack01@hotmail.com>
2019-09-15 13:15:58 -0500

    examen 2

---
+++ horas trabajadas/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace horas_trabajadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txthorastrabajadas = new System.Windows.Forms.TextBox();
+            this.lbltotalsalario = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtvalorhora = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "digite sus horas trabajadas";
+            // 
+            // txthorastrabajadas
+            // 
+            this.txthorastrabajadas.Location = new System.Drawing.Point(226, 70);
+            this.txthorastrabajadas.Name = "txthorastrabajadas";
+            this.txthorastrabajadas.Size = new System.Drawing.Size(100, 20);
+            this.txthorastrabajadas.TabIndex = 2;
+            // 
+            // lbltotalsalario
+            // 
+            this.lbltotalsalario.AutoSize = true;
+            this.lbltotalsalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotalsalario.Location = new System.Drawing.Point(112, 294);
+            this.lbltotalsalario.Name = "lbltotalsalario";
+            this.lbltotalsalario.Size = new System.Drawing.Size(56, 24);
+            this.lbltotalsalario.TabIndex = 3;
+            this.lbltotalsalario.Text = "Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor de las horas";
+            // 
+            // txtvalorhora
+            // 
+            this.txtvalorhora.Location = new System.Drawing.Point(226, 106);
+            this.txtvalorhora.Name = "txtvalorhora";
+            this.txtvalorhora.Size = new System.Drawing.Size(100, 20);
+            this.txtvalorhora.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtvalorhora);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbltotalsalario);
+            this.Controls.Add(this.txthorastrabajadas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txthorastrabajadas;
+        private System.Windows.Forms.Label lbltotalsalario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtvalorhora;
+    }
+}
+
---
+++ horas trabajadas/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace horas_trabajadas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double horastrabajadas = double.Parse(txthorastrabajadas.Text);
+            double valorhora = double.Parse(txtvalorhora.Text);
+            double total = horastrabajadas * valorhora;
+
+            if (total >=1000001)
+            {
+                total = total = (total * 0.3) - total;
+
+                lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
+                lbltotalsalario.Visible = true;
+
+                MessageBox.Show("tienes un descuento del 5% y un aumento del 2%");
+            }
+            else
+             if (total >= 6890000)
+            {
+                total = (total * 0.2) - total;
+                lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
+                lbltotalsalario.Visible = true;
+
+                MessageBox.Show("tienes un descuento del 2%");
+            }
+            else
+             if (total >= 750000)
+            {
+                MessageBox.Show("tienes un aumento del  10%");
+                total = (total * 0.10) + total;
+                lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
+                lbltotalsalario.Visible = true;
+            }
+           
+
+
+
+
+
+
+        }
+    }
+}
