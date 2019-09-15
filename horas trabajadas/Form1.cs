9d3c8341fa534848f3aaf96a3bd17713cc83963f
nasus01 <54506169+nasus01@users.noreply.github.com>
2019-09-15 18:49:19 -0500

    Update Form1.cs

--- horas trabajadas/Form1.cs
+++ horas trabajadas/Form1.cs
@@ -23,31 +23,33 @@ namespace horas_trabajadas
             double valorhora = double.Parse(txtvalorhora.Text);
             double total = horastrabajadas * valorhora;
 
-            if (total >=1000001)
-            {
-                total = (total / 0.5) *  0.2;
-
+            double horastrabajadas = double.Parse(txthorastrabajadas.Text);
+            double valorhora = double.Parse(txtvalorhora.Text);
+            double total = horastrabajadas * valorhora;
+            double desc=valorhora*0.5;
+            if (total <= 700000) {
+                total = total * 0.10+total;
                 lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
                 lbltotalsalario.Visible = true;
 
-                MessageBox.Show("tienes un descuento del 5% y un aumento del 2%");
+                MessageBox.Show("tienes un aumento del 10%");
             }
             else
-             if (total >= 6890000)
             {
-                total = (total * 0.2) - total;
-                lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
-                lbltotalsalario.Visible = true;
-
-                MessageBox.Show("tienes un descuento del 2%");
+                if (total <= 1000000)
+                {
+                    total = (total-desc)-0.3;
+                    lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
+                    lbltotalsalario.Visible = true;
+                    MessageBox.Show("tienes un descueto del 2%");
+                }
             }
-            else
-             if (total >= 750000)
-            {
-                MessageBox.Show("tienes un aumento del  10%");
-                total = (total * 0.10) + total;
+            if (total >= 1000001) {
+
+                total = (total - desc)+0.2;
                 lbltotalsalario.Text = "$" + Convert.ToDecimal(total).ToString("N0");
                 lbltotalsalario.Visible = true;
+                MessageBox.Show("tienes un descueto del 5% y un incremento del 2%");
             }
            
 
